814570aa42462f3ecfffeb3340d87fa2937b803b
her14nto <her14nto@gmail.com>
2016-10-06 11:06:42 +0700

    test twitter api

--- app/Http/Controllers/SNSController.php
+++ app/Http/Controllers/SNSController.php
@@ -24,22 +24,22 @@ class SNSController extends Controller
           'consumer_secret' => "dfKiFTYb2F2Pn2zjQaQP4DOaH7v3VbWY27U0Kkn2ooDSZYKu3q"
         );
 
-        $url = 'https://api.twitter.com/1.1/followers/ids.json';
-        $getfield = '?screen_name=her14nto';
+        $url = 'https://api.twitter.com/1.1/statuses/user_timeline.json';
+        $getfield = '?screen_name=gistaputri&count=20';
         $requestMethod = 'GET';
 
-        $twitter = new \TwitterAPIExchange($settings);
-        echo $twitter->setGetfield($getfield)
-        ->buildOauth($url, $requestMethod)
-        ->performRequest();
+        $twitter = new TwitterAPIExchange($settings);
 
-        dd($twitter);
+        $tweets = json_decode($twitter->setGetfield($getfield)
+             ->buildOauth($url, $requestMethod)
+             ->performRequest(),$assoc = TRUE);
 
         if($request->has('hashtag'))
           $data = \Bolandish\Instagram::getMediaByHashtag($request->get('hashtag'), 10);
         else
           $data = collect([]);//\Bolandish\Instagram::getMediaByHashtag("chalkboard", 10);
-        return view('sosmed_aggregator', compact('data'));
+        return view('sosmed_aggregator', compact('data','tweets'));
+        
         //$clientId = '8de9d9b46e294b0ea8c43efa05c90f31';
         //$clientSecret = 'f5bac935921a452295594f05ab9025ea';
         //$accessToken = '3556328137.8de9d9b.848ed39457b546d480f28a0b22699a2a';
